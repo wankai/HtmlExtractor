99bbaac09f47185f28b8f9f945b2f3c8624eeaa1
Wankai Zhang <wankaizhang@gmail.com>
2017-02-19 20:12:18 +0800

    Update url_pattern.h

--- src/url_pattern.h
+++ src/url_pattern.h
@@ -5,7 +5,23 @@ namespace hex {
 class UrlPattern
 {
  public:
+  // 
+  explicit UrlPattern(const std::string& path_pattern);
+ 
+  // add a regex for some query param, the value
+  // of @query_param must match @value_pattern
+  void AddQuery(const std::string& query_param,
+                const std::string& value_pattern);
+ 
+  // just clear query regexs
+  void ResetQuery();
+ 
+  // clear both path and query regex
+  void Reset();
+ 
  private:
+  std::regex path_regex_;
+  std::vector<std::regex> query_regexs_;
 };
 
 } // namespace hex
